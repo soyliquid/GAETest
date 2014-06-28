9f1171501f45cae48e87cfe6f7b8559ed0a95fa4
soyliquid <soyliquid@gmail.com>
2014-06-28 16:40:53 +0900

    First Commit.

---
+++ Assets/DBModule.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class DBModule
+{
+
+	public static string ServerURL = "http://hogehoge-application.appspot.com/simple_db";
+	public static string Result;
+
+	public static IEnumerator DoPost(string key, string data, Action callback)
+	{
+		WWWForm form = new WWWForm ();
+		form.AddField ("name", key);
+		form.AddField ("data", data);
+		WWW www = new WWW (ServerURL, form);
+		yield return www;
+		if (!string.IsNullOrEmpty (www.error)) {
+			Debug.LogError (www.error);
+			yield break;
+		}
+		Debug.Log("DoPost Done.");
+		if(callback != null) {
+			callback.Invoke();
+		}
+	}
+	
+	public static IEnumerator DoGet(string key, Action callback)
+	{
+		WWW www = new WWW (ServerURL+"?name="+key);
+		yield return www;
+		if (!string.IsNullOrEmpty (www.error)) {
+			Debug.LogError (www.error);
+			yield break;
+		}
+  		Result = www.text;
+		Debug.Log("DoGet Done.");
+		if(callback != null) {
+			callback.Invoke();
+		}
+	}
+
+}
---
+++ Assets/MainManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainManager : MonoBehaviour {
+
+	public string postStatus = "Push to Post";
+	public string getStatus = "Push to Get";
+
+	void OnGUI() {
+		if(GUI.Button(new Rect(0f, 0f, 80f, 30f), "Post")) {
+			string data = Random.Range(-100, 100).ToString("000");
+			postStatus = "Posting... value=" + data;
+			StartCoroutine(DBModule.DoPost("ID", data, PostDone));
+		}
+		if(GUI.Button(new Rect(0f, 30f, 80f, 30f), "Get")) {
+			getStatus = "Getting...";
+			StartCoroutine(DBModule.DoGet("ID", GetDone));
+		}
+		GUI.Label(new Rect(80f, 0f, 80f, 180f), postStatus);
+		GUI.Label(new Rect(80f, 30f, 80f, 180f), getStatus);
+		GUI.Label(new Rect(0f, 60f, 80f, 180f), DBModule.Result);
+	}
+	
+	void PostDone() {
+		postStatus = "Post Done.";
+	}
+
+	void GetDone() {
+		getStatus = "Get Done.";
+	}
+
+}
